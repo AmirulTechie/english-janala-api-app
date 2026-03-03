73c719d3b940a56f0d48b69969cd8138fe8377d1
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-03-03 21:12:25 +0600

    feat: feteched data from words by levels api and lesson cards on the make

--- script/index.js
+++ script/index.js
@@ -2,6 +2,25 @@ const loadLessons=()=>{
     fetch('https://openapi.programming-hero.com/api/levels/all')
     .then(res=>res.json())
         .then(json => displayLessons(json.data))
+};
+const loadLevelWord = (id) =>{
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res=>res.json())
+        .then(data => displayLevelWord(data.data));
+}
+const displayLevelWord=(words)=>{
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = '';
+
+    words.forEach(word => {
+        const card = document.createElement('div');
+        card.innerHTML = `
+         <p>Cat</p>
+        `
+        wordContainer.append(card);
+    })
 }
 const displayLessons =(lessons)=>{
     // 1.get the container & empty it
@@ -10,10 +29,9 @@ const displayLessons =(lessons)=>{
     // 2. get into every lessons
     for(let lesson of lessons){
         // 3. create element
-        console.log(lesson);
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML = `
-            <button class="btn btn-outline btn-primary">
+            <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
             <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
             </button>
         `
