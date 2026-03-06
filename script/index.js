f4ba962a828211a38db4ec3df4d84a620e182b79
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-03-06 23:56:14 +0600

    add text to speech functionality

--- script/index.js
+++ script/index.js
@@ -11,6 +11,11 @@ const manageSpinner =(status)=>{
         document.getElementById('spinner').classList.add('hidden');
     }
 }
+function pronounceWord(word) {
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.lang = "en-EN"; // English
+    window.speechSynthesis.speak(utterance);
+}
 
 const loadLessons=()=>{
     fetch('https://openapi.programming-hero.com/api/levels/all')
@@ -83,7 +88,7 @@ const displayLevelWord=(words)=>{
         <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation?word.pronunciation : "Pronunciation পাওয়া যায়নি" }"</div>
         <div class="flex justify-between items-center">
             <button onClick="loadWordDetail(${word.id})" class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-circle-info"></i></button>
-            <button class="btn  bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-volume-high"></i></button>
+            <button onclick="pronounceWord('${word.word}')" class="btn  bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
     </div>
         `
